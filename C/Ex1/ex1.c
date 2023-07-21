d98688989ade9e586b69a1e7ad12b9d1dd6d102d
hichamoutagra <92459594+hichamoutagra@users.noreply.github.com>
2023-07-21 14:24:16 +0100

    Add files via upload

---
+++ C/Ex1/ex1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //we calculate the multiplication of two numbers
+   int num1;
+   int num2;
+   printf("Enter number 1: ");
+   scanf("%d",&num1);
+   printf("Enter number 2 : ");
+   scanf("%d",&num2);
+   printf("num1 * num2 = %d",num1*num2);
+}
