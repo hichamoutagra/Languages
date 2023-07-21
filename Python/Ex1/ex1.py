a82d16eab7e8ad726f94b61dbd12db49561dcdca
hichamoutagra <92459594+hichamoutagra@users.noreply.github.com>
2023-07-21 13:49:08 +0100

    Add files via upload

---
+++ Python/Ex1/ex1.py
@@ -0,0 +1,16 @@
+print("1.")
+time = 6.892
+distance = 19.7
+speed = distance/ time
+print("The speed is ",speed)
+n = int(input("number of digits after decimal point: "))
+ch ="%."+str(n)+"f"
+print(ch % speed)
+print("----------------------------------------------------")
+print("2.")
+name =input("Enter your name : ")
+age = int(input("Enter your age  : "))
+print("My name is ",name," and My age is ",age)
+raw_input("Nice")
+
+          
